25b2e84ad0732421e8c5497f3c08eb624c5f9a30
rysiekmirek <55394633+rysiekmirek@users.noreply.github.com>
2022-10-04 01:48:30 +0200

    views and tenplate

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schedule-tour/', views.schedule_tour, name = 'schedule-tour' ),
 ]
--- DjangoApp/views.py
+++ DjangoApp/views.py
@@ -1,1 +1,8 @@
-"""Here we put views"""
+from django.shortcuts import render, redirect
+from .models import Tour
+from .forms import TourForm
+import requests
+
+
+def schedule_tour(request):
+    return render (request, "schedule-tour.html")
